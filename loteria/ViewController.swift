f87eb7de6e13c310895a66c6bef213fa31f36652
Jeferson Fernandes <88930674+Mecbossdev@users.noreply.github.com>
2022-07-26 07:21:21 -0300

    Initial Commit

---
+++ loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loteria
+//
+//  Created by jeffin uGostosão da pacatuba on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
